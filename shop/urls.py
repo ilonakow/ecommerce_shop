501ef952f6070a7804f198881016681c3c757f2f
intro_orange <dominikaa.szulc@outlook.com>
2023-05-29 20:34:37 +0200

    create for welcome page, category page and product page and templates html

--- shop/urls.py
+++ shop/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 from shop import views
 
 urlpatterns = [
-    path('welcome/', views.welcome)
+    path('welcome/', views.welcome),
+    path('category/', views.category),
+    path('product/', views.product),
 
 
 
--- shop/views.py
+++ shop/views.py
@@ -5,4 +5,19 @@ from django.shortcuts import HttpResponse
 # Create your views here.
 
 def welcome(request):
-    return HttpResponse("witaj na stronie głównej")
+    return render(
+        request,
+        'shop/Welcome_page.html',
+    )
+
+def category(request):
+    return render(
+        request,
+        'shop/category_page.html',
+    )
+
+def product(request):
+    return render(
+        request,
+        'shop/product_page.html',
+    )
