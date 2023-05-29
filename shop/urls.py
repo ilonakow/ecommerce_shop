fc429a8ac789760882ec3f76547d2c12520e9aac
intro_orange <dominikaa.szulc@outlook.com>
2023-05-29 20:18:06 +0200

    welcome page

--- shop/urls.py
+++ shop/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 
+from shop import views
 
 urlpatterns = [
+    path('welcome/', views.welcome)
+
+
 
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponse
+
 
 # Create your views here.
+
+def welcome(request):
+    return HttpResponse("witaj na stronie głównej")
