718ad54f4dc817246c47e99f2acaac618d68c585
intro_orange <dominikaa.szulc@outlook.com>
2023-05-29 20:52:05 +0200

    create cart_app

--- ecommerce_shop/urls.py
+++ ecommerce_shop/urls.py
@@ -20,5 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('shop/', include('shop.urls')),
+    path('cart/', include('cart.urls')),
+
 
 ]
