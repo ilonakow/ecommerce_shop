0589c631540c38ad4f0c70bff17de6d5215e441e
Ilona Kowalewska <ilona.kowalewska112@gmail.com>
2023-06-07 15:11:27 +0200

    register Order in admin.py

--- shop/admin.py
+++ shop/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from shop.models import Category, Product
+from shop.models import Category, Product, Order
 
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Product)
+admin.site.register(Order)
